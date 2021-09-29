2d89d3aa9b9a457463660fd078e9cefdb7e10572
kamilaanfisa <kamilochka.kast@gmail.com>
2021-09-29 10:40:30 +0300

    <?xml version="1.0" encoding="UTF-8"?> <project version="4"> <component name="VcsDirectoryMapping"> <mapping directory="C:/Users/prepod/AndroidStudioProjects/Planer4Isp932" vcs="Git"> </component> </project>

--- app/src/main/java/com/example/planer4isp9_3/AutoActivity.java
+++ app/src/main/java/com/example/planer4isp9_3/AutoActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class AutoActivity extends AppCompatActivity {
     Button btnLogin;
     Button btnReg;
+    TextView login;
+    TextView password;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +22,19 @@ public class AutoActivity extends AppCompatActivity {
 
         btnLogin=findViewById(R.id.button);
         btnReg = findViewById(R.id.button2);
-
+        login=findViewById(R.id.etLogin);
+        password=findViewById(R.id.etPassword);
     }
     public void BtnClickLogin(View view){
-        Intent intent=new Intent(AutoActivity.this,PlanerActivity.class);
-        startActivity(intent);
+        if (login.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+            Toast.makeText(getApplicationContext(),"Готово!",Toast.LENGTH_SHORT).show();
+            Intent intent=new Intent(AutoActivity.this,PlanerActivity.class);
+            startActivity(intent);
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"Неверно!!!",Toast.LENGTH_SHORT).show();
+        }
+
     }
     public void  BtnClickReg(View view2){
         Intent intent=new Intent(AutoActivity.this,RegActivity.class);
--- app/src/main/java/com/example/planer4isp9_3/PlanerActivity.java
+++ app/src/main/java/com/example/planer4isp9_3/PlanerActivity.java
@@ -1,6 +1,8 @@
 package com.example.planer4isp9_3;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,4 +13,11 @@ public class PlanerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.planerlayout);
     }
+
+    public  void  btnAddClick(View view){
+        TextView textView= new TextView(PlanerActivity.this);
+        textView.setBackgroundResource(R.drawable.zametka);
+        
+    }
+
 }
